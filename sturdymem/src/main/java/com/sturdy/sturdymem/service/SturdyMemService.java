53e083ac6bd3170f325be936640a72901933f29b
vinothchinnasamy <vinothchinnasamy27@gmail.com>
2019-12-14 09:00:06 +0530

    Refactoring Repositories

--- sturdymem/src/main/java/com/sturdy/sturdymem/service/SturdyMemService.java
+++ sturdymem/src/main/java/com/sturdy/sturdymem/service/SturdyMemService.java
@@ -1,16 +1,21 @@
 package com.sturdy.sturdymem.service;
 
 import com.sturdy.sturdymem.entity.MyTransactionResource;
+import org.bson.types.ObjectId;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface SturdyMemService {
 
-    void saveToDictionary(MultipartFile file);
+    //void saveToDictionary(MultipartFile file);
 
     boolean findIfTargetWordExists(String targetWord);
 
     boolean saveMyTransaction(MyTransactionResource myTransactionResource);
-    MyTransactionResource fetchMyTransaction(String transId);
+    //MyTransactionResource fetchMyTransaction(String transId);
     boolean updateMyTransaction(String transId, MyTransactionResource myTransactionResource);
+
+    //Recent additions
+    public MyTransactionResource fetchTransactionByTransId(String transId);
+    public MyTransactionResource fetchTransactionById(ObjectId id);
 
 }
